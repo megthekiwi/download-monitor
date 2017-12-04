48172c9fa4e42084627907068b17a151ad3e0e29
Meg <meg.kurdziolek@gmail.com>
2017-12-04 12:34:27 -0500

    Create new template for image and download count
    Created a new template for displaying image and download count only. Similar to box, but without download-box-content div.

---
+++ templates/content-download-image.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Just image and download count
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+} // Exit if accessed directly
+?>
+<aside class="download-image">
+
+	<a href="<?php $dlm_download->the_download_link(); ?>"><?php $dlm_download->the_image(); ?></a>
+
+	<div
+		class="download-count"><?php printf( _n( '1 download', '%d downloads', $dlm_download->get_the_download_count(), 'download-monitor' ), $dlm_download->get_the_download_count() ) ?></div>
+
+	
+</aside>
